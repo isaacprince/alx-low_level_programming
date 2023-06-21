765646fba14d3cdc2700fcced322c8987fee694a
isaacprince <isaacprince002@gmail.com>
2023-06-21 01:04:57 -0700

    chore: files formated

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,19 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ *   main - check the code
+ * 
+ *   Return: Always 0.
+ */
 
-	/**
-	 * add - adds two integers and returns the result
-	 * @a: int to be added to b
-	 * @b: int to be added to a
-	 * Return: sum of a and b
-	 */
-	int add(int a, int b)
-	{
-		int c;
+int main(void)
+{
+	    int n;
 
+	        n = add(89, 9);
+		printf("%d\n", n);
+		return (0);
 
-		c = a + b;
-
-
-		return (c);
-	}
+}
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @c: The number to be computed.
+ * Return: Absolute value of number or zero
+ */
 
-	/**
-	 * _abs - computes the absolute value of an integer
-	 * @n: the int to check
-	 * Return: the absolute value of int
-	 */
-	int _abs(int n)
+int _abs(int c)
+
+{
+
+	if (c < 0)
 	{
-		if (n >= 0)
-		{
-			return (n);
-		}
-		return (-n);
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
 	}
+	return (c);
+}
